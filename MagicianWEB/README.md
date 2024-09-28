08eb65fdb14da04fa869476af8dc975e6cb54341
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-09-28 15:46:34 -0300

    asd

--- MagicianWEB/README.md
+++ MagicianWEB/README.md
@@ -1,6 +1,6 @@
-# MagicianWEB
+# MagicianWeb
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.1.2.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.6.
 
 ## Development server
 
--- MagicianWEB/src/app/app.component.spec.ts
+++ MagicianWEB/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'MagicianWEB' title`, () => {
+  it(`should have the 'MagicianWeb' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('MagicianWEB');
+    expect(app.title).toEqual('MagicianWeb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MagicianWEB');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, MagicianWeb');
   });
 });
