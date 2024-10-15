450605f1504d24eda6508a2f17776f92724e6777
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-10-15 20:49:25 -0300

    asd

--- MagicianWEB/src/app/pages/home/home.component.ts
+++ MagicianWEB/src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { relative } from 'path';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
-
 }
