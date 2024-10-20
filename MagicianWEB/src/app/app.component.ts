2c90e47dcf8483eca5455323363bb64fe76898aa
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-10-20 19:30:37 -0300

    asd

--- MagicianWEB/src/app/app.component.ts
+++ MagicianWEB/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+      HomeComponent,
+      FooterComponent,
+      HeaderComponent,
+      RouterOutlet
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
