2b201ed2801a72b6c9ac629f8566bfa2b1b109c7
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-10-24 21:15:46 -0300

    asd

--- MagicianWEB/src/app/app.config.server.ts
+++ MagicianWEB/src/app/app.config.server.ts
@@ -1,9 +1,11 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { HomeComponent } from './pages/home/home.component';
 
 const serverConfig: ApplicationConfig = {
   providers: [
+    HomeComponent,
     provideServerRendering()
   ]
 };
