92035da79ecbca1b47876a9e978c5d0248a7f3a0
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-12-10 15:37:22 -0300

    asd

--- MagicianWEB/src/app/app.component.ts
+++ MagicianWEB/src/app/app.component.ts
@@ -5,22 +5,28 @@ import { FooterComponent } from './shared/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ConseguirComponent } from './pages/conseguir/conseguir.component';
+import { ParticlesComponent } from './particles/particles.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  
+  
   imports: [
-      HomeComponent,
-      ProjectsComponent,
-      ConseguirComponent, 
-      FooterComponent,
-      HeaderComponent,
-      RouterOutlet
-    ],
+    AppComponent,
+    HomeComponent,
+    ProjectsComponent,
+    ConseguirComponent, 
+    FooterComponent,
+    HeaderComponent,
+    RouterOutlet,
+    ParticlesComponent, 
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'MagicianWeb';
 }
