c6a37d6ba58e6481d194cc562621dbe2d2d409d5
Jordan Iralde <iraldejordan10@gmail.com>
2024-12-18 17:16:03 -0300

    Construccion

--- MagicianWEB/src/app/app.routes.ts
+++ MagicianWEB/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ConseguirComponent } from './pages/conseguir/conseguir.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { FondoEstrellasComponent } from './pages/fondo-estrellas/fondo-estrellas.component';
+import { ConstruccionComponent } from './pages/construccion/construccion.component';
 
 export const routes: Routes = [
     {path:'' , component : HomeComponent},
@@ -13,4 +14,5 @@ export const routes: Routes = [
     {path:'about', component: AboutComponent},
     {path:'contact', component: ContactComponent},
     {path:"e", component:FondoEstrellasComponent},
+    {path:'construccion', component:ConstruccionComponent},
 ];
