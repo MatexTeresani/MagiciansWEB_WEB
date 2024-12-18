10e591517b439b2fe7a6d1d8ff474cbafe3538c0
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-12-18 16:54:15 -0300

    asd

--- MagicianWEB/src/app/app.routes.ts
+++ MagicianWEB/src/app/app.routes.ts
@@ -2,15 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { ConseguirComponent } from './pages/conseguir/conseguir.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ContactComponent } from './pages/contact/contact.component';
 import { FondoEstrellasComponent } from './pages/fondo-estrellas/fondo-estrellas.component';
 
 export const routes: Routes = [
     {path:'' , component : HomeComponent},
     {path:'projects', component : ProjectsComponent},
     {path:'conseguir', component: ConseguirComponent},
-    {path:'about', component: AboutComponent},
-    {path:'contact', component: ContactComponent},
     {path:"e", component:FondoEstrellasComponent},
 ];
