10b58c81d6959209ac131f26fc185cd0f9cecbf1
Giovanni Peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2024-12-24 16:41:54 -0300

    asd

--- MagicianWEB/src/app/pages/conseguir/conseguir.component.ts
+++ MagicianWEB/src/app/pages/conseguir/conseguir.component.ts
@@ -11,17 +11,36 @@ import { Router } from '@angular/router';
 })
 export class ConseguirComponent {
 
-  constructor(private router: Router) {}
-
-
-  Enviar(){
+  constructor(private router: Router) {
     
+  }
 
+  
+  Enviar(){
+
+    const nombre = document.getElementById('nombre') as HTMLInputElement;
+    const apellido = document.getElementById('apellido') as HTMLInputElement;
+    const email = document.getElementById('email') as HTMLInputElement;
+    const telefono = document.getElementById('telefono') as HTMLInputElement;
+    const empresa = document.getElementById('empresa') as HTMLInputElement;
+    const detalle = document.getElementById('detalles') as HTMLTextAreaElement;
+
+    if (nombre.value == "" || apellido.value == "" || email.value == "" || telefono.value == "" || empresa.value == "" || detalle.value == "") {
+      
+      alert("Por favor, complete todos los campos");
+      return;
+
+    }
+    else{
+
+    
 
      this.router.navigate(['consigue-terminado']).then(() => {
       window.scrollTo(0, 0);
      }); // Navega a la ruta raíz
 
      console.log("Enviado");
-  }
+
+    }
+  }  
 }
