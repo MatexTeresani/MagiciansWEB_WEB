6fd47eb880e65deb32836d1d2c81d1bce49901c4
Jordan Iralde <iraldejordan10@gmail.com>
2025-02-07 18:15:11 -0300

    Componentes

--- MagicianWEB/src/app/pages/home/home.component.ts
+++ MagicianWEB/src/app/pages/home/home.component.ts
@@ -1,25 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { relative } from 'path';
+import { ConseguirComponent } from "../conseguir/conseguir.component";
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ConseguirPaginaComponent } from './conseguir-pagina/conseguir-pagina.component';
+import { MagicianLogoComponent } from './magician-logo/magician-logo.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MagicianLogoComponent,
+    ConseguirPaginaComponent,
+    AboutUsComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
 
-  constructor(private router: Router) {}
-
-
-  consiquehref(){
-     this.router.navigate(['conseguir']).then(() => {
-      window.scrollTo(0, 0);
-     }); // Navega a la ruta raíz
-
-
-  }
 }
---
+++ MagicianWEB/src/app/pages/home/magician-logo/magician-logo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-magician-logo',
+  standalone: true,
+  imports: [],
+  templateUrl: './magician-logo.component.html',
+  styleUrl: './magician-logo.component.css'
+})
+export class MagicianLogoComponent {
+
+}
