33ef4b725b07107ef7b17b754a54534c0daa9f65
Giovanni Peirone <mgpeirone25@gmail.com>
2025-02-11 17:49:02 -0300

    aa

--- MagicianWEB/src/app/pages/conseguir/conseguir.component.ts
+++ MagicianWEB/src/app/pages/conseguir/conseguir.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+
+
 @Component({
   selector: 'app-conseguir',
   standalone: true,
   templateUrl: './conseguir.component.html',
   styleUrls: ['./conseguir.component.css'],
 })
-export class ConseguirComponent {
 
+export class ConseguirComponent {
   Enviar(){
+
+
     console.log("hello");
   }
 }
